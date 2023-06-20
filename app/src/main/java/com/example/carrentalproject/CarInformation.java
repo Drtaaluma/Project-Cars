2869369a0233c0e77c0323bfb0e929aae7dd97ed
Drtaaluma <drtaaluma3236@gmail.com>
2023-06-20 08:46:04 +0300

    Project

---
+++ app/src/main/java/com/example/carrentalproject/CarInformation.java
@@ -0,0 +1,14 @@
+package com.example.carrentalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CarInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_information);
+    }
+}
